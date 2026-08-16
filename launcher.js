93362ce75d429954da4b23ef54a3b27a87b47898
Chris Muha <muhachris297@gmail.com>
2026-08-16 13:02:57 -0400

    Fit square Bingo card content

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-square-player-cards";
+const SITE_BUILD = "20260816-bingo-square-card-content-fit";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
