4ce6d910e1027ab63bab3e6a4b9b9a013abfad7e
Chris Muha <muhachris297@gmail.com>
2026-08-16 12:54:48 -0400

    Contain Classic Bingo desktop controls

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-refresh-state-clock";
+const SITE_BUILD = "20260816-bingo-classic-control-containment";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
