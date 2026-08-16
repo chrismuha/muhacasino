b4ad4686739b97604bbcfb56730c07e152eb7b67
Chris Muha <muhachris297@gmail.com>
2026-08-16 13:45:36 -0400

    Override embedded Classic timezone layout

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-timezone-clean-row";
+const SITE_BUILD = "20260816-bingo-classic-timezone-width";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
