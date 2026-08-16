9bc99e8b5bd55286459e571fc9bdab402621ec5e
Chris Muha <muhachris297@gmail.com>
2026-08-16 13:15:30 -0400

    Compact and surface Bingo timezone control

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-view-top-bar";
+const SITE_BUILD = "20260816-bingo-compact-surfaced-timezone";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
