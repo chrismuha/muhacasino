b8192f4a2692ba6de0eca70f2eacf5eb27802495
Chris Muha <muhachris297@gmail.com>
2026-08-16 12:47:22 -0400

    Fix Classic Bingo toolbar containment

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-planet-hall-2-default";
+const SITE_BUILD = "20260816-bingo-v1-1-11-toolbar-fix";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
