4c97b135f69296bde90753dd6ffaa7547dcfe62b
Chris Muha <muhachris297@gmail.com>
2026-08-16 13:40:49 -0400

    Contain timezone control in time panel

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-timezone-single-control";
+const SITE_BUILD = "20260816-bingo-timezone-contained";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
