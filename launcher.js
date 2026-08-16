b11fb1c12c122a5085ebcf10752823e9ea027a06
Chris Muha <muhachris297@gmail.com>
2026-08-16 13:37:55 -0400

    Fix timezone dropdown styling

--- launcher.js
+++ launcher.js
@@ -79,7 +79,7 @@ const categoryNames = {
   table: "Tabletop Games",
 };
 
-const SITE_BUILD = "20260816-bingo-splat-daub-v1";
+const SITE_BUILD = "20260816-bingo-timezone-single-control";
 const launcher = document.querySelector("#launcher");
 const gameView = document.querySelector("#gameView");
 const gameFrame = document.querySelector("#gameFrame");
