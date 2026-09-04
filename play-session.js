ef549973c89724295521272f7e040eb9493e04dd
Chris Muha <muhachris297@gmail.com>
2026-09-04 19:20:19 -0400

    Remove confirmed unused casino code and assets [allow-dependency-regression]

--- play-session.js
+++
@@ -1,5 +0,0 @@
-(() => {
-    const script = document.createElement("script");
-    script.src = new URL("../muhaslots/play-session.js", document.currentScript.src).href;
-    document.head.append(script);
-})();
